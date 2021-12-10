114946bd976527fc3eb32a58805f05a6214822c7
Septianleonardo <dwigantoroleonardoseptian@gmail.com>
2021-12-10 17:07:10 +0700

    upload final files

---
+++ Assets/Scenes/skor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class skor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int skor = PlayerPrefs.GetInt("skor");
+        GetComponent<Text> ().text = PlayerPrefs.GetInt("skor").ToString();
+        if (skor != 0)
+        {
+            
+            PlayerPrefs.SetInt("skor",0 );
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    
+}
